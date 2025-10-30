7f004bd808fe5809f5ae228bc04088d5b54da13b
BKA121 <khacanhdz@gmail.com>
2025-10-31 00:54:35 +0700

    Tạo BrandMapper

---
+++ backend/product-service/src/main/java/com/vdt2025/product_service/mapper/BrandMapper.java
@@ -0,0 +1,20 @@
+package com.vdt2025.product_service.mapper;
+
+import com.vdt2025.product_service.dto.request.brand.BrandCreationRequest;
+import com.vdt2025.product_service.dto.request.brand.BrandUpdateRequest;
+import com.vdt2025.product_service.dto.response.BrandResponse;
+import com.vdt2025.product_service.entity.Brand;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface BrandMapper {
+    Brand toBrand(BrandCreationRequest request);
+    BrandResponse toBrandResponse(Brand brand);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+
+    void updateBrand(@MappingTarget Brand brand, BrandUpdateRequest request);
+}
