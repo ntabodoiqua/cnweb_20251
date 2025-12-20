ddd91fecb0f1ec19feb79d6725645a37aa814ef9
LeDinhHungAnh <167419005+LeDinhHungAnh@users.noreply.github.com>
2025-12-20 20:20:53 +0700

    feat: Home UI

---
+++ frontend/src/components/home/index.js
@@ -0,0 +1,20 @@
+/**
+ * Home Components Export
+ * Centralized export for all home page components
+ */
+
+export { default as HeroBanner } from "./HeroBanner";
+export { default as PromotionBanners } from "./PromotionBanners";
+export { default as CategoriesSection } from "./CategoriesSection";
+export { default as FlashSaleSection } from "./FlashSaleSection";
+export { default as ProductsSection } from "./ProductsSection";
+export { default as ProductCard } from "./ProductCard";
+export { default as BrandsSection } from "./BrandsSection";
+export { default as TestimonialsSection } from "./TestimonialsSection";
+export { default as VideoReviewsSection } from "./VideoReviewsSection";
+export { default as QuickViewModal } from "./QuickViewModal";
+export { default as TrustBadges } from "./TrustBadges";
+export {
+  default as RecentlyViewed,
+  addToRecentlyViewed,
+} from "./RecentlyViewed";
