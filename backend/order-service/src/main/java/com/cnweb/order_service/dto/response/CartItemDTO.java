b972720fc32c93efd0b2637c498bb7dab1100e44
BKA121 <khacanhdz@gmail.com>
2025-12-24 23:58:22 +0700

    Update response order service

---
+++ backend/order-service/src/main/java/com/cnweb/order_service/dto/response/CartItemDTO.java
@@ -0,0 +1,37 @@
+package com.cnweb.order_service.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CartItemDTO implements Serializable {
+
+    String productId;
+    String productName;
+    String variantId;
+    String variantName;
+    String sku;
+    String imageUrl;
+    String storeName;
+    String storeId;
+    String storeLogo;
+    Integer quantity;
+    BigDecimal price;
+    BigDecimal originalPrice;
+    LocalDateTime addedAt;
+    LocalDateTime updatedAt;
+
+    public BigDecimal getSubtotal() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+}
