3d42e9dc3c8a818c977d1f2e1578ae9938e97f0f
ntabodoiqua <anhnta2004@gmail.com>
2025-12-30 14:18:16 +0700

    feat: Add Cash On Delivery option to PaymentMethod enum

--- backend/order-service/src/main/java/com/cnweb/order_service/enums/PaymentMethod.java
+++ backend/order-service/src/main/java/com/cnweb/order_service/enums/PaymentMethod.java
@@ -1,5 +1,6 @@
 package com.cnweb.order_service.enums;
 
 public enum PaymentMethod {
+    COD,          // Cash On Delivery - Thanh toán khi nhận hàng
     ZALO_PAY      // ZaloPay - Thanh toán qua ZaloPay
 }
